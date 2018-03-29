ea44c4a0baa38330edbdcbaab1306320756a9419
xueqing tu <flyingyizi@gmail.com>
2018-03-30 07:54:58 +0800

    init

---
+++ src/nestStaticlibDemo/nestDemo.hpp
@@ -0,0 +1,4 @@
+
+
+void print(char str[]);
+
---
+++ src/nestdynlibDemo/demo.cc
@@ -0,0 +1,11 @@
+
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+//#include <nestDemo.hpp> 
+
+void print(char str[]) {
+  std::cout << "hello from nestDemo:"<<str<< std::endl;
+}
